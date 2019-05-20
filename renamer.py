49affb5de2b084d59d66597978a8222cd5435350
milomacphail <milomacphail@gmail.com>
2019-05-20 12:14:15 -0400

    Added indexing of photos and month names added

--- renamer.py
+++ renamer.py
@@ -4,15 +4,18 @@ from datetime import datetime
 folder = "/Users/milo/Documents/renamer"
 location = input("Photo location: ")
 files = os.listdir(folder)
+counter = 0;
 
 for filename in files:
+    counter += 1
+    string_counter = str(counter)
     if not filename.startswith('.'):
         file = os.path.join(folder, filename)
 
         m_time = os.path.getmtime(file)
         real_time = datetime.fromtimestamp(m_time)
-        f_time = datetime.strftime(real_time, "%Y-%m-%d %H:%M:%S")
+        f_time = datetime.strftime(real_time, "%Y-%B-%d %H:%M:%S")
 
-        new_filename = f_time + "_" + location + ".jpeg"
+        new_filename = string_counter + "_" + f_time + "_" + location + ".jpeg"
         new_file = os.path.join(folder, new_filename)
         os.rename(file, new_file)
