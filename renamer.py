14c6e40e569d0adfea0894d6bb3d65a9db8d5094
milomacphail <milomacphail@gmail.com>
2019-05-20 13:19:01 -0400

    Handles any file type

--- renamer.py
+++ renamer.py
@@ -11,11 +11,12 @@ for filename in files:
     string_counter = str(counter)
     if not filename.startswith('.'):
         file = os.path.join(folder, filename)
+        extension = os.path.splitext(file)
 
         m_time = os.path.getmtime(file)
         real_time = datetime.fromtimestamp(m_time)
         f_time = datetime.strftime(real_time, "%Y-%B-%d %H:%M:%S")
 
-        new_filename = string_counter + "_" + f_time + "_" + location + ".jpeg"
+        new_filename = string_counter + "_" + f_time + "_" + location + extension[1]
         new_file = os.path.join(folder, new_filename)
         os.rename(file, new_file)
